55175269d97c88db25163f64ad424d4505e5d0af
Supun Kamburugamuva <skamburu@tempest.dsc.soic.indiana.edu>
2015-06-28 23:43:51 -0400

    adding cp

---
+++ cp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp /tmp/matrix/* /N/u/skamburu/data/2004_2014/matrix/
--- slurm.sh
+++ slurm.sh
@@ -5,6 +5,7 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --time=00:30:00
 
-/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --hostfile nodes --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar DistanceCalculator -v /N/u/skamburu/data/2004_2014/vectors -d /tmp/matrix -m
+#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --hostfile nodes --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar DistanceCalculator -v /N/u/skamburu/data/2004_2014/vectors -d /tmp/matrix -m
 
-cp /tmp/matrx/* /N/u/skamburu/data/2004_2014/matrix
+#srun "cp /tmp/matrx/* /N/u/skamburu/data/2004_2014/matrix/"
+srun ./cp.sh 
