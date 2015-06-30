0e9d93521f2c96f25500596fa82606fb306a0bd6
Supun <supun06@gmail.com>
2015-06-30 15:55:46 -0400

    adding vectors

--- mpi/src/main/java/VectorPoint.java
+++ mpi/src/main/java/VectorPoint.java
@@ -29,38 +29,38 @@ public class VectorPoint {
         double []xs = vc.numbers;
         double []ys = this.numbers;
 
-//        PearsonsCorrelation pearsonsCorrelation = new PearsonsCorrelation();
-//        double cor = pearsonsCorrelation.correlation(xs, ys);
-//        return (1 - cor) / 2;
+        PearsonsCorrelation pearsonsCorrelation = new PearsonsCorrelation();
+        double cor = pearsonsCorrelation.correlation(xs, ys);
+        return (1 - cor) / 2;
 
-        double sx = 0.0;
-        double sy = 0.0;
-        double sxx = 0.0;
-        double syy = 0.0;
-        double sxy = 0.0;
-
-        int n = xs.length;
-
-        for(int i = 0; i < n; ++i) {
-            double x = xs[i];
-            double y = ys[i];
-
-            sx += x;
-            sy += y;
-            sxx += x * x;
-            syy += y * y;
-            sxy += x * y;
-        }
-
-        // covariation
-        double cov = sxy / n - sx * sy / n / n;
-        // standard error of x
-        double sigmax = Math.sqrt(sxx / n -  sx * sx / n / n);
-        // standard error of y
-        double sigmay = Math.sqrt(syy / n -  sy * sy / n / n);
-
-        // correlation is just a normalized covariation
-        return (1 -cov / (sigmax * sigmay)) /2;
+//        double sx = 0.0;
+//        double sy = 0.0;
+//        double sxx = 0.0;
+//        double syy = 0.0;
+//        double sxy = 0.0;
+//
+//        int n = xs.length;
+//
+//        for(int i = 0; i < n; ++i) {
+//            double x = xs[i];
+//            double y = ys[i];
+//
+//            sx += x;
+//            sy += y;
+//            sxx += x * x;
+//            syy += y * y;
+//            sxy += x * y;
+//        }
+//
+//        // covariation
+//        double cov = sxy / n - sx * sy / n / n;
+//        // standard error of x
+//        double sigmax = Math.sqrt(sxx / n -  sx * sx / n / n);
+//        // standard error of y
+//        double sigmay = Math.sqrt(syy / n -  sy * sy / n / n);
+//
+//        // correlation is just a normalized covariation
+//        return (1 -cov / (sigmax * sigmay)) /2;
     }
 
     public void add(double number) {
