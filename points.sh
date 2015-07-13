96700ba25a797608043a28c56c9a8000154e19d9
Supun <supun06@gmail.com>
2015-07-12 23:34:35 -0400

    fixing problem with getting most common element

--- points.sh
+++ points.sh
@@ -14,4 +14,28 @@ mkdir -p $CONT_COMMON_POINTS
 #java -cp mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar PVectorGenerator -i $GLOBAL -o $GLOBAL_VECS -d 3000
 
 # generate the common points
-java -cp mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar PointTransformer -g $GLOBAL_VECS/2004_2014.csv -gp $GLOBAL_POINTS/2004_2014.txt -v $CONT_VECS -p $CONT_POINTS -d $CONT_COMMON_POINTS
+#java -cp mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar PointTransformer -g $GLOBAL_VECS/2004_2014.csv -gp $GLOBAL_POINTS/2004_2014.txt -v $CONT_VECS -p $CONT_POINTS -d $CONT_COMMON_POINTS
+
+# rotate the points
+MANXCAT_JAR=/home/supun/dev/projects/dsspidal/rotate/target/mdsaschisq-1.0-ompi1.8.1-jar-with-dependencies.jar
+ROTATE_POINTS=$CONT_COMMON_POINTS/*
+ROTATE_OUT=$BASE_DIR/continous_rotate
+ROTATE_CONTROL=$ROTATE_OUT/rotate
+FULL_POINTS=$CONT_POINTS
+for f in $ROTATE_POINTS
+do
+  common_filename="${f##*/}"
+  common_filenameWithoutExtension="${common_filename%.*}"
+  echo $common_filename
+  common_file=$CONT_COMMON_POINTS/$common_filename
+  no_of_common_lines=`sed -n '$=' $common_file`
+  echo $no_of_common_lines
+
+  ext='.txt'
+  full_file=$FULL_POINTS/$common_filenameWithoutExtension$ext
+  echo $full_file
+  no_of_full_lines=`sed -n '$=' $full_file`
+  echo $no_of_full_lines
+
+  java -DBaseResultDirectoryName=$ROTATE_OUT -DControlDirectoryName=$ROTATE_CONTROL -DInitializationFileName= -cp $MANXCAT_JAR salsa.mdsaschisq.ManxcatCentral -c config.properties -n 1 -t 1
+done
