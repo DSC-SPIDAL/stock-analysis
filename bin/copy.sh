4305e6a57cb29a00428dbd1c01a79eb3849dfed3
Supun <supun06@gmail.com>
2015-07-13 22:57:41 -0400

    fixing problem with getting most common element

---
+++ bin/copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cp ../../W2004_2014/continous_vectors/2004_1_2005_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2005_1_2006_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2006_1_2007_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2007_1_2008_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2008_1_2009_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2009_1_2010_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2010_1_2011_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2011_1_2012_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2012_1_2013_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2013_1_2014_1.csv .
+[skamburu@tempest vectors]$ cp ../../W2004_2014/continous_vectors/2014_1_2015_1.csv .
