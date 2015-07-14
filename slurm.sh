63aa2534dbcb3220c507fbe9722b52eaaaebc22c
Supun <supun06@gmail.com>
2015-07-13 22:22:35 -0400

    fixing problem with getting most common element

--- slurm.sh
+++ slurm.sh
@@ -20,7 +20,7 @@ srun -l cp /N/u/skamburu/data/W2004_2014/2004_2014.csv /scratch/2004_2014/2004_2
 
 
 #/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar DistanceCalculator -v /N/u/skamburu/data/2004_2014/global_vectors -d /tmp/global_matrix_1 -m
-#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar FileBreaker -i /scratch/2004_2014/2004_2014.csv -o /scratch/2004_2014/continous_breaks -s 20040101 -e 20150101 -d 4 -m
+#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar FileBreaker -i /scratch/2004_2014/2004_2014.csv -o /scratch/2004_2014/continous_breaks -s 20040101 -e 20150101 -d 4 -m -t 1
 #srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_breaks
 #srun -l cp -rf /scratch/2004_2014/continous_breaks /N/u/skamburu/data/W2004_2014/
 
