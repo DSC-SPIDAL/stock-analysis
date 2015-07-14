660bee1a31294670de981488b3e948f75557142d
Supun <supun06@gmail.com>
2015-07-13 22:49:07 -0400

    fixing problem with getting most common element

--- slurm.sh
+++ slurm.sh
@@ -5,22 +5,32 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --time=04:00:00
 
+JAR_FILE=/N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar
+
+BASE_DIR=/N/u/skamburu/data/E2004_2014
+VECTOR_DIR=$BASE_DIR/vectors
+MATRIX_DIR=$BASE_DIR/matrix
+WEIGHT_MATRIX_DIR=$BASE_DIR/weight_matrix
+GLOBAL_VEC_DIR=$BASE_DIR/global_vectors
+GLOBA_DIR=$BASE_DIR/global
+GLOBAL_MATRIX_DIR=$BASE_DIR/global_matrix
+
 #srun ./cp_global.sh
 srun -l echo "Hello"
 #srun -l rm -rf /scratch/2004_2014/continous_breaks
 #srun -l rm -rf /scratch/2004_2014/continous_vectors
-srun -l rm -rf /scratch/2004_2014/continous_matrix
+#srun -l rm -rf /scratch/2004_2014/continous_matrix
 #srun -l rm -rf /scratch/2004_2014/continous_weight
 
 #srun -l mkdir -p /scratch/2004_2014/continous_breaks
 #srun -l mkdir -p /scratch/2004_2014/continous_vectors
-srun -l mkdir -p /scratch/2004_2014/continous_matrix
+#srun -l mkdir -p /scratch/2004_2014/continous_matrix
 #srun -l mkdir -p /scratch/2004_2014/continous_weight
-srun -l cp /N/u/skamburu/data/W2004_2014/2004_2014.csv /scratch/2004_2014/2004_2014.csv
+#srun -l cp /N/u/skamburu/data/W2004_2014/2004_2014.csv /scratch/2004_2014/2004_2014.csv
 
 
 #/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar DistanceCalculator -v /N/u/skamburu/data/2004_2014/global_vectors -d /tmp/global_matrix_1 -m
-#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar FileBreaker -i /scratch/2004_2014/2004_2014.csv -o /scratch/2004_2014/continous_breaks -s 20040101 -e 20150101 -d 4 -m -t 1
+#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar FileBreaker -i /scratch/2004_2014/2004_2014.csv -o /scratch/2004_2014/continous_breaks -s 20040101 -e 20150101 -d 4 -m
 #srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_breaks
 #srun -l cp -rf /scratch/2004_2014/continous_breaks /N/u/skamburu/data/W2004_2014/
 
@@ -28,11 +38,11 @@ srun -l cp /N/u/skamburu/data/W2004_2014/2004_2014.csv /scratch/2004_2014/2004_2
 #srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_vectors
 #srun -l cp -rf /scratch/2004_2014/continous_vectors /N/u/skamburu/data/W2004_2014/
 
-/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar DistanceCalculator -v /scratch/2004_2014/continous_vectors -d /scratch/2004_2014/continous_matrix -m
-srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_matrix
-srun -l cp -rf /scratch/2004_2014/continous_matrix /N/u/skamburu/data/W2004_2014/
+/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp $JAR_FILE DistanceCalculator -v $VECTOR_DIR -d $MATRIX_DIR -m -t 1 -s
+#srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_matrix
+#srun -l cp -rf /scratch/2004_2014/continous_matrix /N/u/skamburu/data/W2004_2014/
 
-#/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp /N/u/skamburu/projects/apps/stock-analysis/mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar WeightCalculator -v /scratch/2004_2014/continous_vectors -d /scratch/2004_2014/continous_weight -m -n
+/N/u/skamburu/projects/software/openmpi-1.8.1/build/bin/mpirun --report-bindings --mca btl ^tcp java -cp $JAR_FILE WeightCalculator -v $VECTOR_DIR -d $MATRIX_DIR -m -n
 #srun -l mkdir -p /N/u/skamburu/data/W2004_2014/continous_weight
 #srun -l cp -rf /scratch/2004_2014/continous_weight /N/u/skamburu/data/W2004_2014/
 
