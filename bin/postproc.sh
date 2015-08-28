660c7e88205b0564c6025ebd28a21002466d429c
Supun <supun06@gmail.com>
2015-08-28 18:30:03 -0400

    fixing problem with getting most common element

--- bin/postproc.sh
+++ bin/postproc.sh
@@ -153,3 +153,33 @@ java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar LabelApply
 -o $ORIGINAL_STOCK_FILE \
 -s $HIST_DIR -h
 
+# generate heat maps
+# ----------------------
+
+#YEARLY_MDS_DIR_NAME=/N/u/skamburu/data/2004_2014_AUG_24/mds/unweighted/yearly
+YEARLY_DISTANCES_DIR_NAME=$YEARLY_PREPROC_DIR_NAME/distances
+YEARLY_HEATMAP_DIR=$YEARLY_POSTPROC_DIR_NAME/heatmap
+
+mkdir -p $YEARLY_HEATMAP_DIR
+
+HEAT_POINTS=$YEARLY_MDS_DIR_NAME/*
+for f in $HEAT_POINTS
+do
+  common_filename="${f##*/}"
+  common_filename_ext="${common_filename%.*}"
+  common_file=$YEARLY_MDS_DIR_NAME/$common_filename
+  echo 'common file' $common_file
+  no_of_common_lines=`sed -n '$=' $common_file`
+  echo $no_of_common_lines
+
+  ext='.csv'
+  distance_file=$YEARLY_DISTANCES_DIR_NAME/$common_filename_ext$ext
+
+  echo "distance file" $YEARLY_MDS_DIR_NAME/$common_file
+  echo "point file" $common_file
+  echo "-Drows=$no_of_common_lines -Dcols=$no_of_common_lines -DAmat=$common_file -DBmat=$distance_file -Dtitle=$common_filename -Doutdir=$YEARLY_HEATMAP_DIR -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties"
+
+  java -Drows=$no_of_common_lines -Dcols=$no_of_common_lines -DAmat=$common_file -DBmat=$distance_file -Dtitle=$common_filename_ext -Doutdir=$YEARLY_HEATMAP_DIR -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties
+
+  cat $YEARLY_HEATMAP_DIR/plot.bat >> $YEARLY_HEATMAP_DIR/plot_master.sh
+done
