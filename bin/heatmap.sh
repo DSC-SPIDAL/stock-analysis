4bd5c9761dfa68d6ba405186ac5a7af32b86f1f2
Supun Kamburugamuva <skamburu@tempest.dsc.soic.indiana.edu>
2015-08-28 18:03:11 -0400

    adding

--- bin/heatmap.sh
+++ bin/heatmap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this is a test file so that I can integrate this later to postproc.sh
-YEARLY_MDS_DIR_NAME=/N/u/skamburu/data/2004_2014_AUG_24/preproc/yearly/distances
+YEARLY_MDS_DIR_NAME=/N/u/skamburu/data/2004_2014_AUG_24/mds/unweighted/yearly
 YEARLY_DISTANCES_DIR_NAME=/N/u/skamburu/data/2004_2014_AUG_24/preproc/yearly/distances
 YEARLY_HEATMAP_DIR=/N/u/skamburu/data/2004_2014_AUG_24/postproc/unweighted/yearly/heatmap
 
@@ -17,14 +17,12 @@ do
 
   ext='.csv'
   distance_file=$YEARLY_DISTANCES_DIR_NAME/$common_filename_ext$ext
+  
+  echo "distance file" $YEARLY_MDS_DIR_NAME/$common_file
+  echo "point file" $common_file
+  echo "-Drows=$no_of_common_lines -Dcols=$no_of_common_lines -DAmat=$common_file -DBmat=$distance_file -Dtitle=$common_filename -Doutdir=$YEARLY_HEATMAP_DIR -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties"
 
-  java -Drows $no_of_common_lines \
-   -Dcols $no_of_common_lines \
-   -DAmat  $YEARLY_MDS_DIR_NAME\common_filename \
-   -DBmat  $YEARLY_DISTANCES_DIR_NAME\$distance_file \
-   -Dtitle $common_filename  \
-   -Doutdir $YEARLY_HEATMAP_DIR  \
-   -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties
+  java -Drows=$no_of_common_lines -Dcols=$no_of_common_lines -DAmat=$common_file -DBmat=$distance_file -Dtitle=$common_filename_ext -Doutdir=$YEARLY_HEATMAP_DIR -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties
 
   cat $YEARLY_HEATMAP_DIR/plot.bat >> $YEARLY_HEATMAP_DIR/plot_master.sh
 done
