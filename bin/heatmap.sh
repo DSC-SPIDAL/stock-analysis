72f87788480b2cc7c97f107de7b566a910ca6ba8
supunkamburugamuva <supun06@gmail.com>
2015-08-28 14:34:55 -0400

    adding the program

--- bin/heatmap.sh
+++ bin/heatmap.sh
@@ -18,14 +18,13 @@ do
   ext='.csv'
   distance_file=$YEARLY_DISTANCES_DIR_NAME/$common_filename_ext$ext
 
-  java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator \
-   -c heatmap.properties \
-   -Drows $no_of_common_lines \
+  java -Drows $no_of_common_lines \
    -Dcols $no_of_common_lines \
    -DAmat  $YEARLY_MDS_DIR_NAME\common_filename \
    -DBmat  $YEARLY_DISTANCES_DIR_NAME\$distance_file \
    -Dtitle $common_filename  \
-   -Doutdir $YEARLY_HEATMAP_DIR
+   -Doutdir $YEARLY_HEATMAP_DIR  \
+   -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar SHeatMapGenerator -c heatmap.properties
 
   cat $YEARLY_HEATMAP_DIR/plot.bat >> $YEARLY_HEATMAP_DIR/plot_master.sh
 done
