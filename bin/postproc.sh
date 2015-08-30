2c99794102be793b59ab89fb4eaba1d10493ca6e
Supun <supun06@gmail.com>
2015-08-29 23:01:54 -0400

    fixing problem with getting most common element

--- bin/postproc.sh
+++ bin/postproc.sh
@@ -27,6 +27,7 @@ GLOBAL_MDS_DIR_NAME=$MDS_DIR_NAME/global
 #### names of directories inside the base dir
 # directory where histograms are created
 HIST_DIR_NAME=$POSTPROC_INTERMEDIATE_DIR_NAME/histogram
+GLOBAL_HIST_DIR_NAME=$POSTPROC_INTERMEDIATE_DIR_NAME/histogram/global
 # directory where the global vectors
 GLOBAL_VEC_DIR_NAME=$GLOBAL_PREPROC_DIR_NAME/vectors
 # directory where the global stock file with all stocks for that period
@@ -46,6 +47,11 @@ ROTATE_FINAL_SUMMARY_DIR_NAME=$YEARLY_POSTPROC_DIR_NAME/rotate/summary
 # directory where final labeled point output
 LABEL_OUT_DIR_NAME=$YEARLY_POSTPROC_DIR_NAME/rotate/points/labeled/byhist
 SECTOR_LABEL_OUT_DIR_NAME=$YEARLY_POSTPROC_DIR_NAME/rotate/points/labeled/bysec
+
+GLOBAL_FINAL_POINTS_DIR=$GLOBAL_POSTPROC_DIR_NAME/points
+GLOBAL_LABEL_OUT_DIR_NAME=$GLOBAL_POSTPROC_DIR_NAME/points/labeled/byhist
+GLOBAL_SECTOR_LABEL_OUT_DIR_NAME=$GLOBAL_POSTPROC_DIR_NAME/labeled/points/bysec
+
 # directory where output of rotation program stored
 ROTATE_OUT_DIR_NAME=$POSTPROC_INTERMEDIATE_DIR_NAME/rotate_ops
 # name of the global stock file name
@@ -56,6 +62,7 @@ GLOBAL_POINTS_FILE_NAME=2004_2014.txt
 mkdir -p $BASE_DIR/$ROTATE_FINAL_DIR_NAME
 mkdir -p $BASE_DIR/$ROTATE_FINAL_SUMMARY_DIR_NAME
 mkdir -p $BASE_DIR/$LABEL_OUT_DIR_NAME
+mkdir -p $BASE_DIR/$GLOBAL_HIST_DIR_NAME
 
 MANXCAT_JAR=$HOME/.m2/repository/com/google/guava/guava/15.0/guava-15.0.jar:$HOME/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:$HOME/.m2/repository/habanero-java-lib/habanero-java-lib/0.1.4-SNAPSHOT/habanero-java-lib-0.1.4-SNAPSHOT.jar:$HOME/.m2/repository/ompi/ompijavabinding/1.8.1/ompijavabinding-1.8.1.jar:$HOME/.m2/repository/org/jblas/jblas/1.2.3/jblas-1.2.3.jar:/$HOME/.m2/repository/edu/indiana/salsahpc/mdsaschisq/1.0-ompi1.8.1/mdsaschisq-1.0-ompi1.8.1.jar
 
@@ -72,6 +79,7 @@ CONT_POINTS=$BASE_DIR/$POINTS_DIR_NAME
 GLOBAL_CONT_COMMON_POINTS=$BASE_DIR/$GLOBAL_COMMON_POINTS_DIR_NAME
 CONT_COMMON_POINTS=$BASE_DIR/$COMMON_POINTS_DIR_NAME
 HIST_DIR=$BASE_DIR/$HIST_DIR_NAME
+GLOBAL_HIST_DIR=$BASE_DIR/$GLOBAL_HIST_DIR_NAME
 
 mkdir -p $CONT_COMMON_POINTS
 mkdir -p $GLOBAL_CONT_COMMON_POINTS
@@ -84,7 +92,7 @@ mv $CONT_COMMON_POINTS/2004_2014.csv $GLOBAL_CONT_COMMON_POINTS
 # generate histogram
 # --------------------
 java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar Histogram -v $CONT_VECS -s $ORIGINAL_STOCK_FILE -d $HIST_DIR -b 10 | tee $BASE_DIR/$POSTPROC_INTERMEDIATE_DIR_NAME/histogram.out.txt
-
+java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar Histogram -v $GLOBAL_POINTS_DIR_NAME -s $ORIGINAL_STOCK_FILE -d $GLOBAL_HIST_DIR -b 10 | tee $BASE_DIR/$POSTPROC_INTERMEDIATE_DIR_NAME/global_histogram.out.txt
 # rotate the points
 # ******************
 ROTATE_POINTS=$CONT_COMMON_POINTS/*
@@ -144,6 +152,11 @@ do
     mv "$i" "`echo $i | sed 's/full\.txt//'`"
 done
 cd $dir
+
+# copyt the global points
+mkdir -p $BASE_DIR/$GLOBAL_FINAL_POINTS_DIR
+cp -r $GLOBAL_POINTS/* $BASE_DIR/$GLOBAL_FINAL_POINTS_DIR
+
 LABEL_OUT=$BASE_DIR/$LABEL_OUT_DIR_NAME
 mkdir -p $LABEL_OUT
 echo "APPLY HIST LABELS"
@@ -154,6 +167,16 @@ java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar LabelApply
 -o $ORIGINAL_STOCK_FILE \
 -s $HIST_DIR -h
 
+GLOBAL_LABEL_OUT=$BASE_DIR/$GLOBAL_LABEL_OUT_DIR_NAME
+mkdir -p $GLOBAL_LABEL_OUT
+echo "APPLY GLOABL HIST LABELS"
+java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar LabelApply \
+-v $CONT_VECS \
+-p $BASE_DIR/$GLOBAL_POINTS_DIR_NAME \
+-d $GLOBAL_LABEL_OUT \
+-o $ORIGINAL_STOCK_FILE \
+-s $GLOBAL_HIST_DIR -h
+
 echo "APPLY SECTOR LABELS"
 SEC_LABEL_OUT=$BASE_DIR/$SECTOR_LABEL_OUT_DIR_NAME
 mkdir -p $SEC_LABEL_OUT
@@ -163,6 +186,15 @@ java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar LabelApply
 -v $CONT_VECS \
 -p $FINAL_ROTATE \
 -d $SEC_LABEL_OUT \
+-o $ORIGINAL_STOCK_FILE \
+-s $SEC_FILE
+
+GLOBAL_SEC_LABEL_OUT=$BASE_DIR/$GLOBAL_SECTOR_LABEL_OUT_DIR_NAME
+mkdir -p $GLOBAL_SEC_LABEL_OUT
+java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar LabelApply \
+-v $CONT_VECS \
+-p $BASE_DIR/$GLOBAL_POINTS_DIR_NAME \
+-d $GLOBAL_SEC_LABEL_OUT \
 -o $ORIGINAL_STOCK_FILE \
 -s $SEC_FILE
 
