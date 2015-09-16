933e9df05b6bdcd5d4688e30801733962799c986
Supun <supun06@gmail.com>
2015-09-16 16:56:32 -0400

    generating simple weight file

--- mpi/src/main/java/trfm/DistanceTransformer.java
+++ mpi/src/main/java/trfm/DistanceTransformer.java
@@ -5,7 +5,8 @@ import edu.indiana.soic.spidal.common.TransformationFunction;
 public class DistanceTransformer implements TransformationFunction {
     @Override
     public double transform(double val) {
-        double convert = 2 - 2 * val;
-        return Math.sqrt(2 * (1 - convert));
+//        double convert = 2 - 2 * val;
+//        return Math.sqrt(2 * (1 - convert));
+        return val;
     }
 }
