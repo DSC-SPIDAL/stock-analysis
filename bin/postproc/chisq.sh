fa598db9dd210f4e9570270d63ac8f30980a83fc
Supun <supun06@gmail.com>
2015-09-29 23:57:18 -0400

    remove constant vector

--- bin/postproc/chisq.sh
+++ bin/postproc/chisq.sh
@@ -4,6 +4,8 @@ SUMMARY=$1
 
 for f in $SUMMARY/*
 do
+    common_filename="${f##*/}"
+    common_filename_ext="${common_filename%.*}"
     v=`tac $f | grep -m 1 "Best Chisq" | awk '/^\Chisq/ {gsub(//,""); print $2}'`
-    echo $f","$v >> out.txt
+    echo $common_filename_ext","$v >> out.txt
 done
