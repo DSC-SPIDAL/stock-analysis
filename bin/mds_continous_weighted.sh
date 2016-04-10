1833f8b3f1b7886229269f5674d0ff1654038080
Supun <supun06@gmail.com>
2016-04-10 00:16:15 -0400

    moving to nio for file write

--- bin/mds_continous_weighted.sh
+++ bin/mds_continous_weighted.sh
@@ -10,6 +10,8 @@ fi
 BASE_DIR=$1
 PREPROC_DIR=$BASE_DIR/preproc
 
+ORIGINAL_STOCK_FILE=$BASE_DIR/input/2004_2015.csv
+
 # no need to change the below lines
 YEARLY_PREPROC_DIR=$PREPROC_DIR/yearly
 GLOBAL_PREPROC_DIR=$PREPROC_DIR/global
@@ -17,6 +19,8 @@ GLOBAL_PREPROC_DIR=$PREPROC_DIR/global
 MDS_DIR=$BASE_DIR/mds/weighted
 YEARLY_MDS_DIR=$MDS_DIR/yearly
 GLOBAL_MDS_DIR=$MDS_DIR/global
+INTER_MDS_DIR=$MDS_DIR/intermediate
+INTER_POINT_DIR=$INTER_MDS_DIR/points
 
 POINTS_DIR=$YEARLY_MDS_DIR
 MATRIX_DIR=$YEARLY_PREPROC_DIR/distances
@@ -32,19 +36,33 @@ GLOBAL_DAMDS_SUMMARY_DIR=$GLOBAL_MDS_DIR/summary
 
 mkdir -p $DAMDS_SUMMARY_DIR
 mkdir -p $GLOBAL_DAMDS_SUMMARY_DIR
+mkdir -p $INTER_POINT_DIR
 
 MATRIX_FILES=$MATRIX_DIR/*
 VECTOR_BASE=$VECTOR_DIR/
 
-for f in $MATRIX_FILES
-do
-  filename="${f##*/}"
-  filenameWithoutExtension="${filename%.*}"
-  echo $filename
-  vf=$VECTOR_BASE$filename
-  echo $vf
-  no_of_lines=`sed -n '$=' $vf`
-  echo $no_of_lines
-  sbatch internal_mds_weighted.sh $f $no_of_lines $POINTS_DIR/$filenameWithoutExtension $WEIGHTS_DIR/$filename $DAMDS_SUMMARY_DIR/$filenameWithoutExtension
-done
-
+echo "Generating list"
+basefile=
+#java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar CCommonGenerator -v $VECTOR_DIR -p $INTER_POINT_DIR -d $INTER_MDS_DIR -r $YEARLY_MDS_DIR -sd 20040101 -ed 20150101 -l -md 5 | tee $BASE_DIR/$INTER_MDS_DIR/mds.list.out.txt
+java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar PVizFileListGenerator -s 20040101 -e 20151231 -d 9 -o $INTER_MDS_DIR/list.txt -ext "" -i $ORIGINAL_STOCK_FILE | tee $BASE_DIR/$INTER_MDS_DIR/mds.list.out.txt
+TXT_EXT='.txt'
+FILE_LIST=$INTER_MDS_DIR/list.txt
+{
+  read;
+  while read line; do
+      filename=$line$TXT_EXT
+      echo $filename
+      vf=$VECTOR_BASE$filename
+      echo $vf
+      no_of_lines=`sed -n '$=' $vf`
+      echo $no_of_lines
+      java -cp ../mpi/target/stocks-1.0-ompi1.8.1-jar-with-dependencies.jar MDSPointGenerator -v $VECTOR_DIR -p $INTER_POINT_DIR -r $YEARLY_MDS_DIR -ff $line | tee $BASE_DIR/$INTER_MDS_DIR/mds.list.out.txt
+      # first one
+      if [ -z "$basefile" ]; then
+        sbatch internal_mds_weighted.sh $f $no_of_lines $POINTS_DIR/$line $WEIGHTS_DIR/$filename $DAMDS_SUMMARY_DIR/$line
+      else
+        sbatch internal_mds_weighted.sh $f $no_of_lines $POINTS_DIR/$line $WEIGHTS_DIR/$filename $DAMDS_SUMMARY_DIR/$line $INTER_POINT_DIR/$filename
+      fi
+      basefile=$line
+  done
+} < ${FILE_LIST}
--- mpi/src/main/java/MDSPointGenerator.java
+++ mpi/src/main/java/MDSPointGenerator.java
@@ -6,52 +6,28 @@ import java.util.*;
 public class MDSPointGenerator {
     private String pointFolder;
     private String vectorFolder;
-    private String destFolder;
-    private String rotateFolder;
+    private String mdsFolder;
     private String firstFile;
     private String secondFile;
-    private int mode = 4;
-    private boolean listGen = false;
-    private Date startDate;
-    private Date endDate;
-    private String inputFile;
 
     public static void main(String[] args) {
         Options options = new Options();
         options.addOption("v", true, "Input Vector folder");
-        options.addOption("i", true, "Input file");
         options.addOption("p", true, "Points folder");
-        options.addOption("d", true, "Destination point folder");
-        options.addOption(Utils.createOption("r", true, "Rotations folder", true));
+        options.addOption(Utils.createOption("r", true, "MDS folder", true));
         options.addOption(Utils.createOption("ff", true, "First file", false));
         options.addOption(Utils.createOption("sf", true, "Second file", false));
-        options.addOption(Utils.createOption("l", false, "Generate file list", false));
-        options.addOption(Utils.createOption("sd", true, "Start date", false));
-        options.addOption(Utils.createOption("ed", true, "End date", false));
-        options.addOption(Utils.createOption("md", true, "End date", false));
 
         CommandLineParser commandLineParser = new BasicParser();
         try {
             CommandLine cmd = commandLineParser.parse(options, args);
             String vectorFolder = cmd.getOptionValue("v");
-            String inputFile = cmd.getOptionValue("i");
             String pointsFolder = cmd.getOptionValue("p");
-            String distFolder = cmd.getOptionValue("d");
             String rotateFolder = cmd.getOptionValue("r");
             String firstFile = cmd.getOptionValue("ff");
             String secondFile = cmd.getOptionValue("sf");
-            String startDate = cmd.getOptionValue("sd");
-            String endDate = cmd.getOptionValue("ed");
-            String mode = cmd.getOptionValue("md");
-            boolean listGen = cmd.hasOption("l");
-            CCommonGenerator pointTransformer = new CCommonGenerator(pointsFolder, vectorFolder,
-                    distFolder, rotateFolder, firstFile, secondFile, listGen, inputFile);
-            if (listGen) {
-                if (mode != null) {
-                    pointTransformer.setMode(Integer.parseInt(mode));
-                }
-                pointTransformer.setDates(Utils.parseDateString(startDate), Utils.parseDateString(endDate));
-            }
+            MDSPointGenerator pointTransformer = new MDSPointGenerator(pointsFolder, vectorFolder,
+                    rotateFolder, firstFile, secondFile);
             pointTransformer.process();
         } catch (ParseException e) {
             e.printStackTrace();
@@ -61,47 +37,23 @@ public class MDSPointGenerator {
     }
 
     public MDSPointGenerator(String pointFolder, String vectorFolder,
-                            String destPointFolder, String rotateFolder,
-                            String firstFile, String secondFile, boolean listGen, String inputFile) {
+                            String mdsFolder,
+                            String firstFile, String secondFile) {
         this.pointFolder = pointFolder;
         this.vectorFolder = vectorFolder;
-        this.destFolder = destPointFolder;
         this.firstFile = firstFile;
         this.secondFile = secondFile;
-        this.rotateFolder = rotateFolder;
-        this.listGen = listGen;
-        this.inputFile = inputFile;
-    }
-
-    public void setMode(int mode) {
-        this.mode = mode;
-    }
-
-    public void setDates(Date start, Date end) {
-        this.startDate = start;
-        this.endDate = end;
+        this.mdsFolder = mdsFolder;
     }
 
     public void process() throws FileNotFoundException {
-        if (!listGen) {
-            processPair(firstFile, secondFile);
-        } else {
-            List<Date> dates;
-            if (mode == 6 || mode == 9 || mode == 10) {
-                Set<Date> dateSet = DateUtils.retrieveDates(inputFile);
-                dates = DateUtils.sortDates(dateSet);
-            } else {
-                dates = new ArrayList<Date>();
-            }
-            List<String> list = DateUtils.genDateList(startDate, endDate, mode, dates);
-            writeList(list, destFolder + "/list.txt");
-        }
+        processPair(firstFile, secondFile);
     }
 
     private void processPair(String firstFileWithoutExt, String secondfFileWithoutExt) {
         String firstVectorFileName = this.vectorFolder + "/" + firstFileWithoutExt  + ".csv";
         String secondVectorFileName = this.vectorFolder + "/" + secondfFileWithoutExt  + ".csv";
-        String firstPointFileName = this.rotateFolder + "/" + firstFileWithoutExt + ".txt";
+        String firstPointFileName = this.mdsFolder + "/" + firstFileWithoutExt + ".txt";
         String secondPointFileName = this.pointFolder + "/" + secondfFileWithoutExt + ".txt";
 
         System.out.println("Processing pair: " + firstVectorFileName + " " + secondVectorFileName + " " + firstPointFileName + " " + secondPointFileName);
